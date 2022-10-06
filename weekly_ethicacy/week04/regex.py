b8fa6cf56a526055b06aac094060a09b45370f8d
Amanda Lee <107557711+AmaneWei@users.noreply.github.com>
2022-10-06 21:41:37 +0000

    fixed pattern

--- weekly_ethicacy/week04/regex.py
+++ weekly_ethicacy/week04/regex.py
@@ -1,28 +1,31 @@
 import re
 
-
 def find_name(line):
-    pattern ="[a-l]|\ADr.[^e]|\Mr."
+    # random
+    pattern ="Mr|Ms|Mrs\.?s[A-Z]\w"
     result = re.findall(pattern,line)
-
+    # FirstName and LastName
+    pattern = r"(\w+) (\w+)[^\.]"
+    result = result + re.findall(pattern,line)
+    
     return result
 
 
 f = open("names.txt")
 for line in f.readlines():
-    print(line)
+    # print(line)
     result = find_name(line)
     if (len(result)>0):
         print(result)
 
 
 # other patterns
-# FirstName and LastName
-  # pattern = r"(\w+) (\w+)[^\.]"
-
 # FirstName MiddleName LastName
   # pattern=r'(\w+) (\w+) (\w+)'
 
 # FirstName M. LastName
   # pattern=r'(\w+) (\w+\.) (\w+)'
   # result = result + re.findall(pattern,line)
+
+
+# Mr|Ms|Mrs\.?s[A-Z]\w*
